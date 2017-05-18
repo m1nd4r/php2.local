8529a93553e88b2a170e91df788ce200bcab5f5e
m1nd4r <Rg99868c>
2017-05-18 09:30:25 +0400

    + lesson 4

--- test.php
+++ test.php
@@ -3,16 +3,3 @@
 
 require __DIR__ . '/autoload.php';
 
-function first()
-{
-    echo 'First';
-    return false;
-}
-
-function second()
-{
-    echo 'Second';
-    return true;
-}
-
-var_dump(second() xor first());
